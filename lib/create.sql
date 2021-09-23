b6f2d0790c9a237c9e08cd6180564fb2dd94cf48
Madison <madivallarino@gmail.com>
2021-09-23 11:04:47 -0400

    commit tree be47939188af4bf41ebeef486aa6f48221257414

---
+++ lib/create.sql
@@ -0,0 +1,24 @@
+
+-- what I did: low key fak u 
+CREATE TABLE bears (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER,
+sex CHAR(1),
+color TEXT,
+temperament TEXT,
+alive BOOLEAN
+);
+
+
+-- lol below is what the solution did, fak u 
+-- CREATE TABLE bears (
+--   id INTEGER PRIMARY KEY,
+--   name TEXT,
+--   age INTEGER,
+--   sex CHAR(1),
+--   color TEXT,
+--   temperament TEXT,
+--   alive BOOLEAN
+-- );
+
---
+++ lib/insert.sql
@@ -0,0 +1,28 @@
+
+-- FIRST ATTEMPT, APPARENTLY DONT NEED TO ADD THE LIST OF VALUES BEFORE HAND
+-- INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, "Mr. Chocolate", 10, "M", "chocolate brown", "lazy", 1);
+-- INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, "Rowdy", 2, "F", "red-orange", "anxious", 1);
+-- INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, "Tabitha", 20, "F", "grey", 1);
+-- INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, "Sergeant Brown", 100, "M", "black", "serious", 1);
+-- INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, "Melissa", 28, "F", "red head", "ditsy", 1);
+-- INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, "Grinch", 17, "M", "green", "resentful", 1);
+-- INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, "Wendy", 8, "F", "freckles", "innocent", 1);
+-- INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, NULL, 5, "F", "white", "shy", 1);
+
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Mr. Chocolate', 9, 'M', 'chocolate brown', 'lazy', 1);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Rowdy', 2, 'F', 'red orange', 'anxious', 1);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Tabitha', 2, 'F', 'grey', 1);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Sergeant Brown', 1, 'M', 'black', 'serious', 0);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Melissa', 2, 'F', 'red head', 'ditsy', 1);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Grinch', 1, 'M', 'green', 'resentful', 1);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Wendy', 8, 'F', 'freckles', 'innocent', 1);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES(null, 5, 'M', 'white', 'shy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Mr Chocolate', 5, 'M', 'brown', 'lazy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Rowdy', 6, 'F', 'black', 'anxious', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Tabitha', 2, 'F', 'white', 'sleepy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Sergeant Brown', 15, 'M', 'brown', 'dead', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Melissa', 5, 'F', 'brindle', 'happy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Grinch', 7, 'M', 'brown', 'mischeivious', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES('Wendy', 2, 'F', 'white', 'lazy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES(null, 7, 'M', 'black', 'lazy', 0);
